f8791752d08f6b62a25ea05b66fb68334f53ed0f
Tejas Avhad <codingwithtejas7@gmail.com>
2026-06-03 19:22:53 +0530

    feat: implement Edit Project page with form for project updates

--- src/app/dashboard/edit/[id]/page.tsx
+++ src/app/dashboard/edit/[id]/page.tsx
@@ -1,3 +1,43 @@
-export default function EditProjectPage() {
-  return <h1>Edit Project Page</h1>;
+import { getProjectById } from "@/actions/project.actions";
+import { updateProject } from "@/actions/project.actions";
+
+type Props = {
+    params: {id: string}
+}
+
+export default async function EditProjectPage({ params }: Props) {
+    const project = await getProjectById(params.id);
+
+    if (!project) {
+        return <div>Project not found</div>
+    }
+
+      return (
+    <div className="max-w-2xl mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-6">
+        Edit Project
+      </h1>
+
+      <form action={updateProject.bind(null, project.id)}className="space-y-4" >
+        <input name="name" defaultValue={project.name} className="w-full border p-3 rounded"/>
+
+        <textarea name="description" defaultValue={project.description} className="w-full border p-3 rounded"/>
+
+        <input name="url" defaultValue={project.url} className="w-full border p-3 rounded"/>
+
+        <select name="category" defaultValue={project.category}className="w-full border p-3 rounded">
+          <option value="SAAS">SAAS</option>
+          <option value="AI">AI</option>
+          <option value="WEB">WEB</option>
+          <option value="MOBILE">MOBILE</option>
+          <option value="TOOL">TOOL</option>
+          <option value="OTHER">OTHER</option>
+        </select>
+
+        <button className="px-4 py-2 border rounded">
+          Update Project
+        </button>
+      </form>
+    </div>
+  );
 }
